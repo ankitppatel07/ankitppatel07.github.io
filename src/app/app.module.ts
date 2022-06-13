1c72f95fee2dc523a338a5f53e0faeadf2cfdd91
ankitppatel07 <ankitppatel07@gmail.com>
2022-06-12 23:52:38 -0400

    homepage left side components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LeftSideComponent } from './left-side/left-side.component';
+import { RightSideComponent } from './right-side/right-side.component';
+import { SkillsComponent } from './right-side/skills/skills.component';
+import { ExperienceComponent } from './right-side/experience/experience.component';
+import { CertificationsComponent } from './right-side/certifications/certifications.component';
+import { EducationComponent } from './left-side/education/education.component';
+import { SocialComponent } from './left-side/social/social.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LeftSideComponent,
+    RightSideComponent,
+    SkillsComponent,
+    ExperienceComponent,
+    CertificationsComponent,
+    EducationComponent,
+    SocialComponent
   ],
   imports: [
     BrowserModule
